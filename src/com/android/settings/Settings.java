44b2040e37b808025ab4df7c20c3064a3eed296c
Christian Troy <justcavalli@gmail.com>
2012-05-26 15:14:52 +0200

    add ethernet services

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -594,6 +594,7 @@ public class Settings extends PreferenceActivity implements ButtonBarHandler {
     public static class StorageSettingsActivity extends Settings { /* empty */ }
     public static class WifiSettingsActivity extends Settings { /* empty */ }
     public static class WifiP2pSettingsActivity extends Settings { /* empty */ }
+    public static class EthernetSettingsActivity extends Settings { /* empty */ }
     public static class InputMethodAndLanguageSettingsActivity extends Settings { /* empty */ }
     public static class InputMethodAndSubtypeEnablerActivity extends Settings { /* empty */ }
     public static class SpellCheckersSettingsActivity extends Settings { /* empty */ }
