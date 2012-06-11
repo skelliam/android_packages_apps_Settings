d747236c597bf96af68ac937dd53ee24a8d56ad0
Christian Troy <justcavalli@gmail.com>
2012-06-11 14:51:36 +0200

    add special gsensor coordinates workaround
    Conflicts:
    
    	res/values/strings.xml
    	res/xml/display_settings.xml
    	src/com/android/settings/DisplaySettings.java

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -54,6 +54,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final String KEY_ELECTRON_BEAM_CATEGORY_ANIMATION = "category_animation_options";
     private static final String KEY_VOLUME_WAKE = "pref_volume_wake";
     private static final String KEY_HDMI_RESOLUTION = "hdmi_resolution";
+    private static final String KEY_ACCELEROMETER_COORDINATE = "accelerometer_coordinate";
 
     private static final String ROTATION_ANGLE_0 = "0";
     private static final String ROTATION_ANGLE_90 = "90";
@@ -74,6 +75,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private PreferenceScreen mAutomaticBacklightPreference;
 
     private ListPreference mHdmiResolution;
+    private ListPreference mAccelerometerCoordinate;
 
     private ContentObserver mAccelerometerRotationObserver = new ContentObserver(new Handler()) {
         @Override
@@ -127,6 +129,23 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             }
         }
 
+        mHdmiResolution = (ListPreference) findPreference(KEY_HDMI_RESOLUTION);
+        if(mHdmiResolution != null){
+            mHdmiResolution.setOnPreferenceChangeListener(this);
+            String value = Settings.System.getString(getContentResolver(),
+                    Settings.System.HDMI_RESOLUTION);
+            mHdmiResolution.setValue(value);
+            updateHdmiResolutionSummary(value);
+        }
+
+        mAccelerometerCoordinate = (ListPreference) findPreference(KEY_ACCELEROMETER_COORDINATE);
+        if(mAccelerometerCoordinate != null){
+        	mAccelerometerCoordinate.setOnPreferenceChangeListener(this);
+        	String value = Settings.System.getString(getContentResolver(),
+        		Settings.System.ACCELEROMETER_COORDINATE);
+        	mAccelerometerCoordinate.setValue(value);
+        	updateAccelerometerCoordinateSummary(value);
+        }
 
         mElectronBeamAnimationOn = (CheckBoxPreference) findPreference(KEY_ELECTRON_BEAM_ANIMATION_ON);
         mElectronBeamAnimationOff = (CheckBoxPreference) findPreference(KEY_ELECTRON_BEAM_ANIMATION_OFF);
@@ -307,6 +326,17 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             }
         }
     }
+    
+    private void updateAccelerometerCoordinateSummary(Object value){       
+        CharSequence[] summaries = getResources().getTextArray(R.array.accelerometer_summaries);
+        CharSequence[] values = mAccelerometerCoordinate.getEntryValues();
+        for (int i=0; i<values.length; i++) {
+            if (values[i].equals(value)) {
+                mAccelerometerCoordinate.setSummary(summaries[i]);
+                break;
+            }
+        }
+    }
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
@@ -349,9 +379,19 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
                 Settings.System.putString(getContentResolver(), 
                         Settings.System.HDMI_RESOLUTION, value);
                 updateHdmiResolutionSummary(objValue);
-            }catch (NumberFormatException e) {
+            } catch (NumberFormatException e) {
                 Log.e(TAG, "could not persist key hdmi resolution setting", e);
             }
+        }
+        if (KEY_ACCELEROMETER_COORDINATE.equals(key)) {
+        	String value = String.valueOf(objValue);
+        	try {
+        		Settings.System.putString(getContentResolver(),
+        			Settings.System.ACCELEROMETER_COORDINATE, value);
+        		updateAccelerometerCoordinateSummary(objValue);
+        	} catch (NumberFormatException e) {
+        		Log.e(TAG, "could not persist key accelerometer coordinate setting", e);
+        	}
         }
         return true;
     }
